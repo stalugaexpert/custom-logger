2d4b6e37bb611e2786621a953085811569720ae3
stalugaexpert <stalegapawel@gmail.com>
2023-04-21 09:04:47 +0200

    Setup Package

---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+import { LogLevel } from './index'
+
+export type Environment = 'development' | 'production'
+
+export const APP_ENV: Environment =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development'
+
+export const LOG_LEVEL: LogLevel = APP_ENV === 'production' ? 'warn' : 'log'
---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { LOG_LEVEL } from './environment'
+
+export type LogFn = (message?: any, ...optionalParams: any[]) => void
+
+export interface Logger {
+  log: LogFn
+  warn: LogFn
+  error: LogFn
+}
+
+export type LogLevel = 'log' | 'warn' | 'error'
+
+const NO_OP: LogFn = (_message?: any, ..._optionalParams: any[]) => {}
+
+export class ConsoleLogger implements Logger {
+  readonly log: LogFn
+  readonly warn: LogFn
+  readonly error: LogFn
+
+  constructor(options?: { level?: LogLevel }) {
+    const { level } = options || {}
+
+    this.error = console.error.bind(console)
+
+    if (level === 'error') {
+      this.warn = NO_OP
+      this.log = NO_OP
+
+      return
+    }
+
+    this.warn = console.warn.bind(console)
+
+    if (level === 'warn') {
+      this.log = NO_OP
+
+      return
+    }
+
+    this.log = console.log.bind(console)
+  }
+}
+
+export const logger = new ConsoleLogger({ level: LOG_LEVEL })
