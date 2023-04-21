4a4c5b814cc4f3d2b1daa833a82a66a96ebf21de
stalugaexpert <stalegapawel@gmail.com>
2023-04-21 13:27:58 +0200

    Updated env settings

--- src/environment.ts
+++ src/environment.ts
@@ -5,4 +5,4 @@ export type Environment = 'development' | 'production'
 export const APP_ENV: Environment =
   process.env.NODE_ENV === 'production' ? 'production' : 'development'
 
-export const LOG_LEVEL: LogLevel = APP_ENV === 'production' ? 'warn' : 'log'
+export const LOG_LEVEL: LogLevel = APP_ENV === 'production' ? 'error' : 'log'
